68e762c8c3964b2ffb2eff83b9598cbe2b7c3d3e
Tran Quang Tuyen <127078719+TQTuyen@users.noreply.github.com>
2024-11-14 23:46:31 +0700

    SHOP-51 fix provider (#60)

--- frontend/src/context/GlobalProvider.tsx
+++ frontend/src/context/GlobalProvider.tsx
@@ -2,18 +2,27 @@ import React, { ReactNode } from 'react';
 import { AuthProvider } from './AuthContext';
 import { RegisterProvider } from './RegisterContext';
 import { UserProvider } from './UserContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import { store } from '../service/state/store';
 
 interface GlobalProviderProps {
   children: ReactNode;
 }
 
 const GlobalProvider: React.FC<GlobalProviderProps> = ({ children }) => {
+  const queryClient = new QueryClient();
+
   return (
-    <AuthProvider>
-      <RegisterProvider>
-        <UserProvider>{children}</UserProvider>
-      </RegisterProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RegisterProvider>
+            <UserProvider>{children}</UserProvider>
+          </RegisterProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 };
 
