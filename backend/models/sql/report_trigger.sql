8bec48fae4bdde364fea66183789606cf96d91d8
Vivian Bui <thuyvy29032004@gmail.com>
2024-11-19 12:51:00 +0700

    SHOP-59 create report schemas

---
+++ backend/models/sql/report_trigger.sql
@@ -0,0 +1,73 @@
+---------- report status
+CREATE TYPE report_result_status AS ENUM ('Resolved', 'Rejected', 'Invalid');
+
+
+CREATE OR REPLACE FUNCTION fn_check_report_result_status()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT (NEW."result" IN ('Resolved', 'Rejected', 'Invalid')) THEN
+        RAISE EXCEPTION 'Invalid status: %', NEW."result";
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE TRIGGER tg_before_insert_report_result
+BEFORE INSERT ON "ReportResult"
+FOR EACH ROW
+EXECUTE FUNCTION fn_check_report_result_status();
+
+
+
+
+---------------------------------type report
+CREATE TYPE report_type AS ENUM ('Shop', 'Product');
+
+CREATE OR REPLACE FUNCTION fn_check_report_type()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT (NEW."type" IN ('Shop', 'Product')) THEN
+        RAISE EXCEPTION 'Invalid type: %', NEW."type";
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE TRIGGER tg_before_insert_report_result
+BEFORE INSERT ON "Report"
+FOR EACH ROW
+EXECUTE FUNCTION trg_check_report_type();
+
+
+
+--------------------check category
+CREATE OR REPLACE FUNCTION fn_validate_category_based_on_type()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW."type" = 'shop' THEN
+        IF NEW."category" NOT IN ('post banned product ', 'signs of fraud', 'post fake product', 'spam message','Transaction outside shopaa','privacy violation','Vulgar and offensive content','other' ) THEN
+            RAISE EXCEPTION 'Invalid category "%", expected one of: post banned product, signs of fraud, post fake product, spam message, Transaction outside shopaa, privacy violation, Vulgar and offensive content,other', NEW."category";
+        END IF;
+    END IF;
+
+    IF NEW."type" = 'product' THEN
+        IF NEW."category" NOT IN ('signs of fraud', 'fake product', 'origin unknown', 'Vulgar and offensive content','wrong description','signs of virtual single increase','Transaction outside shopaa','other', 'post banned product' ) THEN
+            RAISE EXCEPTION 'Invalid category "%", expected one of: signs of fraud, fake product, origin unknown, Vulgar and offensive content, wrong description, signs of virtual single increase, Transaction outside shopaa, other, post banned product', NEW."category";
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER tg_validate_category_trigger
+BEFORE INSERT OR UPDATE ON "Report"
+FOR EACH ROW
+EXECUTE FUNCTION fn_validate_category_based_on_type();
