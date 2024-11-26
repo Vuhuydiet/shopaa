a3837d88f41ce091dcd4a57409e0caabade69a56
thanhtruc <151757544+blue182@users.noreply.github.com>
2024-11-26 16:14:58 +0700

    Shop 58 update upload product form (#71)
    * show list product shop
    
    * ui header
    
    * fix show shop page
    
    ---------
    
    Co-authored-by: 22120393 <thanhtruc2004pye@gmail.com>

--- frontend/src/components/Shop/HeaderShop.tsx
+++ frontend/src/components/Shop/HeaderShop.tsx
@@ -4,32 +4,49 @@ import './HeaderShopStyle.css';
 
 interface HeaderShopProps {
   shopInfo: any;
+  shopManagerInfo: any;
 }
 
-const HeaderShop: React.FC<HeaderShopProps> = ({ shopInfo }) => {
+const HeaderShop: React.FC<HeaderShopProps> = ({
+  shopInfo,
+  shopManagerInfo,
+}) => {
   return (
     <>
       <div className="HeaderShop">
         <div className="HeaderShop__info">
           <Avatar
-            size={60}
-            src="#"
-            icon={<UserOutlined />}
+            src={
+              shopManagerInfo && shopManagerInfo?.avatarImage?.url
+                ? shopManagerInfo?.avatarImage?.url
+                : '#'
+            }
+            icon={shopManagerInfo?.avatarImage?.url ? null : <UserOutlined />}
             style={{ background: '#ddd', color: '#000' }}
+            className="logo-shop"
           />
           <div style={{ color: '#fff' }}>{shopInfo.shopName}</div>
         </div>
         <div className="HeaderShop__infoDetail">
           <div className="HeaderShop__item">
-            <ProductOutlined style={{ color: '#FF6600', fontSize: '24px' }} />
+            <ProductOutlined
+              style={{ color: '#FF6600', fontSize: '24px' }}
+              className="icon-header-shop"
+            />
             <div>Product 244</div>
           </div>
           <div className="HeaderShop__item">
-            <StarOutlined style={{ color: '#FFFF00', fontSize: '24px' }} />
+            <StarOutlined
+              style={{ color: '#FFFF00', fontSize: '24px' }}
+              className="icon-header-shop"
+            />
             <div>4.8 (72.9k reviews)</div>
           </div>
           <div className="HeaderShop__item">
-            <UserOutlined style={{ color: '#0033FF', fontSize: '24px' }} />
+            <UserOutlined
+              style={{ color: '#0033FF', fontSize: '24px' }}
+              className="icon-header-shop"
+            />
             <div>Join 5 years ago</div>
           </div>
         </div>
