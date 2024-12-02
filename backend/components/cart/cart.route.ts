1df1b2818a2bacab112c6b666a4966b450911a18
vuhuydiet <99078820+Vuhuydiet@users.noreply.github.com>
2024-12-03 00:48:12 +0700

    Shop 64 cart api (#87)
    * SHOP-64 add cart api
    
    * SHOP-64 update add cart item constaint
    
    * update cart api
    
    * SHOP-64 fix cart api (tested)

---
+++ backend/components/cart/cart.route.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import cartController from './cart.controller';
+const router = express.Router();
+
+import passport from '../../libraries/auth/authentication.middleware';
+import Auth from '../../libraries/auth/authorization.middleware';
+import { Role } from '@prisma/client';
+import { body, param, query } from 'express-validator';
+import { handleValidationErrors } from '../../libraries/validator/validator';
+
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  Auth.authorize([Role.USER]),
+
+  body('productId').isNumeric(),
+  body('color').optional().isString(),
+  body('size').optional().isString(),
+  handleValidationErrors,
+
+  cartController.createCartItem
+);
+
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  Auth.authorize([Role.USER]),
+
+  query('limit').optional().isNumeric().toInt(),
+  query('offset').optional().isNumeric().toInt(),
+  handleValidationErrors,
+
+  cartController.getCartItems
+);
+
+router.delete(
+  '/:cartItemId',
+  passport.authenticate('jwt', { session: false }),
+  Auth.authorize([Role.USER]),
+
+  param('cartItemId').isNumeric().toInt(),
+  handleValidationErrors,
+  
+  cartController.deleteCartItem
+);
+
+export default router;
